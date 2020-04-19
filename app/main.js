672bb2b64d74f9607f647680f48f6e5eeaaffc90
Chris <zurad92@gmail.com>
2020-04-19 20:21:08 +0200

    Don't use the server at the moment

--- app/main.js
+++ app/main.js
@@ -1,21 +1,24 @@
-import 'babel-polyfill';
-import 'three';
-import Voxel from './js/model/Voxel';
-import VoxelGridController from './js/controller/VoxelGridController';
-import RemoteClient from './js/remote/RemoteClient';
-import VoxelGridRemoteMediator from './js/remote/VoxelGridRemoteMediator';
+import "babel-polyfill";
+import "three";
+// import Voxel from "./js/model/Voxel";
+import ChessboardController from "./js/controller/ChessboardController";
+// import RemoteClient from "./js/remote/RemoteClient";
+import ChessboardViewMediator from "./js/view/mediator/ChessboardViewMediator";
+import Chessboard from "./js/model/Chessboard";
 
-const CommandSerializer = require('./js/remote/CommandSerializer');
-const VoxelGrid = require('./js/model/VoxelGrid');
+// const CommandSerializer = require("./js/remote/CommandSerializer");
 
-const voxelGrid = new VoxelGrid(50, 40);
+const chessboard = new Chessboard(8, 8);
+const controller = new ChessboardController(
+  chessboard,
+  new ChessboardViewMediator(chessboard)
+);
+// const remoteClient = new RemoteClient('ws://localhost:8081', new CommandSerializer(voxelGrid));
 
-const remoteClient = new RemoteClient('ws://localhost:8081', new CommandSerializer(voxelGrid));
+// remoteClient.addObserver("Connected", (e) => {
+//     console.log('connected');
+//     const voxelGridRemoteMediator = new VoxelGridRemoteMediator(voxelGrid, remoteClient);
+//     const voxelGridController = new VoxelGridController(voxelGrid, voxelGridRemoteMediator);
+// });
 
-remoteClient.addObserver("Connected", (e) => {
-    console.log('connected');
-    const voxelGridRemoteMediator = new VoxelGridRemoteMediator(voxelGrid, remoteClient);
-    const voxelGridController = new VoxelGridController(voxelGrid, voxelGridRemoteMediator);
-});
-
-remoteClient.connect();
+// remoteClient.connect();
